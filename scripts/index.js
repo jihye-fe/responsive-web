cc56e3de2d516fa0b1033ad89585ea3a3becb629
Jihye <jihey6998@gmail.com>
2026-05-14 12:23:03 +0900

    메인 배너 디테일 / 실시간 정책 검색 구조 변경(2단 해야 해서) 완료

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,7 @@ var swiper = new Swiper(".mySwiper", {
     },
     autoplay: {
         delay: 2500,
-        disableOnInteraction: false,
+        disableOnInteraction: true,
     },
     pagination: {
         el: ".page",
